1997d9b1dbeee2dc112aba8ac74157fa107073d0
ruthge <100565660+ruthge@users.noreply.github.com>
2025-09-25 11:34:39 +0300

    firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// הגדרות Firebase 
+window.firebaseConfig = {
+  apiKey: "AIzaSyB8D7kr-ClxQbFDod7A13Xb7Xc87DndHEs",
+  authDomain: "my-first-app-d4cab.firebaseapp.com",
+  projectId: "my-first-app-d4cab",
+  storageBucket: "my-first-app-d4cab.firebasestorage.app",
+  messagingSenderId: "299034871339",
+  appId: "1:299034871339:web:ee08b1fe2f792872080c6e"
+};
